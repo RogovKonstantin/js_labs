f9b6ef33b4b0ca3c5e79d7738595912025dc7fac
kostya <d507756310@gmail.com>
2024-02-24 13:57:18 +0300

    m

--- js6_3.js/accordion.js
+++ js6_3.js/accordion.js
@@ -1,14 +1,12 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const button = document.querySelector('.head .button');
-    const extra = document.getElementById('extra');
-    
-    button.addEventListener('click', () => {
-        if (!extra.style.display || extra.style.display === 'none') {
-            extra.style.display = 'block';
-            button.textContent = 'Less';
-        } else {
-            extra.style.display = 'none';
-            button.textContent = 'More';
-        }
-    });
-});
+function toggle() {
+    var extra = document.getElementById('extra');
+    var button = document.querySelector('.button');
+
+    if (extra.style.display === 'none') {
+        extra.style.display = 'block';
+        button.textContent = 'Less';
+    } else {
+        extra.style.display = 'none';
+        button.textContent = 'More';
+    }
+}
--- js6_5.js/solution.js
+++ js6_5.js/solution.js
@@ -2,7 +2,20 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
-
+      const searchText = document.getElementById('searchField').value.toLowerCase();
+      const rows = document.querySelectorAll('.container tbody tr');
+      
+      rows.forEach(row => {
+         row.classList.remove('select');
+         const cells = Array.from(row.children);
+         for (let i = 0; i < cells.length; i++) {
+            let cell = cells[i];
+            let cellText = cell.textContent.toLowerCase();
+            if (cellText.includes(searchText)) {
+                row.classList.add('select');
+                break; 
+            }
+        }
+      });
    }
 }
