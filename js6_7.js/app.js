c2562aea16da78a1d4fbd3a6ed90b57f9d3c38c5
kostya <d507756310@gmail.com>
2024-02-25 18:37:52 +0300

    m

--- js6_7.js/app.js
+++ js6_7.js/app.js
@@ -1,3 +1,30 @@
 function lockedProfile() {
-    console.log('TODO...')
+   
+    let profiles = document.getElementsByClassName('profile');
+ 
+    for (let profile of profiles) {
+        
+        let button = profile.getElementsByTagName('button')[0];       
+        button.addEventListener('click', function() {
+           
+            let unlockRadio = profile.querySelector('input[value="unlock"]');
+            let lockRadio = profile.querySelector('input[value="lock"]');            
+            let hiddenFields = profile.getElementsByTagName('div')[0];
+
+            if (unlockRadio.checked) {
+                
+                if (hiddenFields.style.display === 'none') {
+                    hiddenFields.style.display = 'block';
+                    button.textContent = 'Hide it';
+                } else {
+                    hiddenFields.style.display = 'none';
+                    button.textContent = 'Show more';
+                }
+            } else if (lockRadio.checked && button.textContent === 'Hide it') {
+                button.disabled = true;
+            } else {
+                button.disabled = false;
+            }
+        });
+    }
 }
--- ls6_8.js/dropdown.js
+++ ls6_8.js/dropdown.js
@@ -1,3 +1,19 @@
 function addItem() {
-    console.log('TODO:...');
+   
+    var textInput = document.getElementById('newItemText');
+    var valueInput = document.getElementById('newItemValue');
+
+    var select = document.getElementById('menu');
+    var option = document.createElement('option');
+
+    option.text = textInput.value + " " + valueInput.value;
+    option.value = valueInput.value;
+
+    for (var i = 0; i < select.options.length; i++) {
+        if (select.options[i].text == option.text) {
+            alert("Duplicate"); 
+            return;
+        }
+    }
+    select.add(option);
 }
